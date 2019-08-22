5b656bb3c116455affe0022df177e88af39dc8f6
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-22 16:03:10 -0400

    and I OOP

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -7,15 +7,13 @@ class PowersController < ApplicationController
 
 
           def show  
-
-          end   
-          
-          
-          def new   
             @power = Power.new  
           end   
+          
+             
         
         end
 
 
         #I was going to work on the power show page. Oh shoott @power oesn't exist yet I need ot fix hat
+        #wait kidding ahh but it's int he wrogn spot
