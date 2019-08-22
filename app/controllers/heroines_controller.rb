273dee0d1a56a276650a916b45604b78d33ba416
benson19 <tomaffew@me.com>
2019-08-22 16:04:50 -0400

    please work

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,27 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine , only: [:show]
+  
+  #all heroines
   def index
     @heroines = Heroine.all
   end
+
+  #view heroine
+  def show
+  end
+
+  #creates new heroine
+  def new
+    @heroine = Heroine.new
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name)
+   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,20 @@
 class PowersController < ApplicationController
+  before_action :find_power , only: [:show]
+  
   def index
     @powers = Power.all
   end
+
+  def show
+  end
+
+  private
+
+  def find_power
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+   end
 end
