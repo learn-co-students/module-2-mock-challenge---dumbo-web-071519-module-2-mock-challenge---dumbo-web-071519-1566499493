1b27926169e93ffa5d84be6e17ee463d7ce69f48
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-23 07:43:42 -0400

    redid the challenge on the train this morning, finished in 38 minutes and change! you know i had to do it to em

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,43 @@
-class HeroinesController < ApplicationController
-  def index
-    @heroines = Heroine.all
-  end
+class HeroinesController < ApplicationController  
+    before_action :find_heroine, only: [:show, :destroy]   
+
+    def index    
+      @heroines = Heroine.all  
+    end   
+
+    def show  
+    end   
+
+    def new   
+      @heroine = Heroine.new 
+      @powers = Power.all 
+    end   
+
+  def create    
+    @heroine = Heroine.create(heroine_params)    
+      if @heroine.valid?      
+          @heroine.errors      
+          redirect_to @heroine    
+      else      
+      flash[:errors] = @heroine.errors.full_messages      
+      redirect_to new_heroine_path    
+     end  
+    end   
+
+
+  def destroy    
+      @heroine.destroy    
+      redirect_to heroines_path  
+    end
+
+
+    private   
+
+    def heroine_params    
+      params.require(:heroine).permit(:name, :super_name, :power_id)  
+    end   
+    
+    def find_heroine    
+      @heroine = Heroine.find(params[:id])  
+    end 
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,23 @@
-class PowersController < ApplicationController
-  def index
-    @powers = Power.all
-  end
+class PowersController < ApplicationController  
+  before_action :find_power, only: [:show, :edit, :update, :destroy]   
+  def index    
+     @powers = Power.all  
+  end  
+
+
+
+  def show  
+    
+  end   
+
+ 
+
+
+
+
+  private   
+   
+  def find_power    
+     @power = Power.find(params[:id])  
+  end 
 end
